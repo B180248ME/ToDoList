c15ed636894dcbf41f4d7f85c544f068a4027266
Bijja Kavya <Bijja.Kavya@lntinfotech.com>
2023-10-02 21:59:04 +0530

    list created and css file added

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+listed_items = [
+    {
+        'name':'first-item',
+        'id':1,
+        'checked':False
+    },
+    {
+        'name':'second-item',
+        'id':2,
+        'checked':True
+    }
+]
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("index.html")
+    return render_template("index.html",items=listed_items)
 
 if __name__ == "__main__":
     app.run(debug=True)
