956614f1aa26832f4f7b64f4e3398f6310f67f5e
Bijja Kavya <Bijja.Kavya@lntinfotech.com>
2023-10-02 22:16:33 +0530

    new items added

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,url_for,request
+import random
 app = Flask(__name__)
 
 listed_items = [
@@ -13,9 +14,18 @@ listed_items = [
         'checked':True
     }
 ]
-@app.route("/")
-@app.route("/home")
+@app.route("/",methods = ['GET','POST'])
+@app.route("/home",methods = ['GET','POST'])
 def home():
+    if request.method == 'POST':
+        item_name= request.form['todo_name']
+        new_id = random.randint(1,1000)
+        new_item = {
+            'name':item_name,
+            'id':new_id,
+            'checked':False
+        }
+        listed_items.append(new_item)
     return render_template("index.html",items=listed_items)
 
 if __name__ == "__main__":
